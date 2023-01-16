119ed52ba60fe1c72d54ca3e4ee0a7e7c876761f
Daniel Sutcliffe <daniel.sutcliffe@co-operativebank.co.uk>
2023-01-16 09:11:14 +0000

    first commit

---
+++ REST/REST/REST.h
@@ -0,0 +1,18 @@
+//
+//  REST.h
+//  REST
+//
+//  Created by Daniel Sutcliffe on 16/01/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for REST.
+FOUNDATION_EXPORT double RESTVersionNumber;
+
+//! Project version string for REST.
+FOUNDATION_EXPORT const unsigned char RESTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <REST/PublicHeader.h>
+
+
